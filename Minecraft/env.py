2959386e3333813dbeb76ff4b73b17f879792209
KBoumghar <Karim.boumghar@umontreal.ca>
2022-05-18 14:28:55 -0400

    Boiler plate functions

--- Minecraft/env.py
+++ Minecraft/env.py
@@ -14,7 +14,12 @@ class ENVB(SimpleEmbodimentEnvSpec):
 
         super().__init__(*args, max_episode_steps=ENV_LENGTH, reward_threshold=100, **kwargs)
 
-    # def create_server_world_generators(self) -> List[Handler]:
+    def create_server_world_generators(self) -> List[Handler]:
+        return [
+            # Basic FlatWorldGenerator for now, could use different biomes.
+            # Check how generator string works in future
+            handlers.FlatWorldGenerator(True)
+        ]
 
     def create_observables(self) -> List[Handler]:
         return super().create_observables() + [  # Coordinates and lifestats
@@ -34,3 +39,31 @@ class ENVB(SimpleEmbodimentEnvSpec):
             handlers.SpawningInitialCondition(True),
             handlers.WeatherInitialCondition(weather)
         ]
+
+    def create_agent_handlers(self) -> List[Handler]:
+        # For now, agent doesn't stop from nothing but number of steps
+        return []
+
+    def create_actionables(self) -> List[Handler]:
+        return super().create_actionables() + [
+            handlers.KeybasedCommandAction("use"),
+
+            # Do we need to allow it to equip each item individually?
+            handlers.EquipAction()
+        ]
+
+    def create_server_quit_producers(self) -> List[Handler]:
+        return []
+
+    def create_server_decorators(self) -> List[Handler]:
+        return []
+
+    def determine_success_from_rewards(self, rewards: list) -> bool:
+        return sum(rewards) >= self.reward_threshold
+
+    def is_from_folder(self, folder: str) -> bool:
+        return folder == 'Minecraft'
+
+    def get_docstring(self):
+        return ""
+
